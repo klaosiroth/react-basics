162410256441ba0d94d899e1c664c33db380d591
Natthaphon Wichianphong <klaosiroth@gmail.com>
2022-08-23 16:24:42 +0700

    setup header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import reactLogo from '../assets/react.svg';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="Header">
+      <img src={reactLogo} className="Header__logo" alt="React Logo" />
+      <span>React Basics</span>
+    </header>
+  );
+}
